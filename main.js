9b41678952f27eabbbfd7316a983c0ebc4557232
CusteredCookieIII <105683628+CusteredCookieIII@users.noreply.github.com>
2023-06-12 15:53:03 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+objects = [];
+status="";
+video = "";
+
+function preload(){
+    video = createVideo('video.mp4');
+    video.hide();
+}
+
+function setup(){
+    canvas = createCanvas(480,380);
+    canvas.center();
+}
+function start()
+{
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "status : Decting Objects"; 
+}
+
+function modelLoaded() {
+    console.log("Model Loaded!");
+    status = true;
+    video.loop();
+    video.speed(1);
+    video.volume(0);
+}
+
+
+
+
+
+
+
+function draw(){
+    image(video, 0, 0, 480, 380);
+    if(status != "")
+    {
+        objectDetector.detect(video,gotResult);
+    }
+}
